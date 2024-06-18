ef6e73dcd3060b90e424d98f911be7ae8abc4fd5
Nikita Filimonov <filimonovnikitav@gmail.com>
2024-06-18 03:23:21 +0300

    Leson 8

---
+++ src/Games/brain-progression.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Braingames\Games\progression;
+
+use function cli\line;
+use function Braingames\Games\Engine\startGame;
+
+function starprogression()
+{
+    $welcomeMessage = "What number is missing in the progression?";
+
+    $questionAnswerPairs = [];
+    for ($i = 0, $numberOfQuestions = 3; $i < $numberOfQuestions; $i += 1) {
+        $arrayNumber = rand(5, 10);
+        $step = rand(1,10);
+        $arrayFirstNumber = rand(0, 100);
+        $arrayAllNumbers = [];
+        for ($x = 0, $curentNumber=$arrayFirstNumber; $x < $arrayNumber; $x += 1){
+            $arrayAllNumbers[] = $curentNumber;
+            $curentNumber += $step;
+        }
+
+        $arrayPosQuestion = rand(1, $arrayNumber);
+        $answer = $arrayAllNumbers[$arrayPosQuestion];
+        $arrayAllNumbers[$arrayPosQuestion] = "..";
+        $question = implode(",", $arrayAllNumbers);
+        $questionAnswerPairs[] = [$question,(string) $answer];
+    }
+    startGame($welcomeMessage, $questionAnswerPairs);
+}
